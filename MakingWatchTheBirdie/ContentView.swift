775a7a19abfa9e65aabc72e6265b79c00d30801a
Andrey Zarembo <andrey.zarembo@gmail.com>
2023-07-24 15:12:43 +0300

    Empty Xcode project for future posts

---
+++ MakingWatchTheBirdie/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MakingWatchTheBirdie
+//
+//  Created by Andrey Zarembo-Godzyatskiy on 24.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MakingWatchTheBirdie/MakingWatchTheBirdieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakingWatchTheBirdieApp.swift
+//  MakingWatchTheBirdie
+//
+//  Created by Andrey Zarembo-Godzyatskiy on 24.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct MakingWatchTheBirdieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
